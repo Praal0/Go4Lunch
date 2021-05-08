dd63835192a2fb3c9646280074ea267c359a34ee
Praal0 <guerrero.dorian1@gmail.com>
2021-05-08 09:50:04 +0200

    P7_Dorian_Guerrero_LoginEmail
    Add :
    Login With Email

--- app/src/main/java/com/example/go4lunch/LoginActivity.java
+++ app/src/main/java/com/example/go4lunch/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.go4lunch;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
@@ -13,6 +14,8 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Collections;
 
@@ -21,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
     private Button btnGoogle;
     private Button btnFacecebook;
+    private Button btnEmail;
     private CoordinatorLayout coordinatorLayout;
 
     @Override
@@ -32,8 +36,13 @@ public class LoginActivity extends AppCompatActivity {
         innit();
         googleBtnClick();
         facebookBtnClick();
-
+        emailBtnClick();
     }
+
+    @Nullable
+    protected FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
+
+    protected Boolean isCurrentUserLogged(){ return (this.getCurrentUser() != null); }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -45,58 +54,70 @@ public class LoginActivity extends AppCompatActivity {
         btnGoogle = findViewById(R.id.google_btn);
         btnFacecebook = findViewById(R.id.facebook_btn);
         coordinatorLayout = findViewById(R.id.login_activity_constraint_layout);
+        btnEmail = findViewById(R.id.email_btn);
     }
 
+    private void emailBtnClick() {
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {startSignInEmail();}
+        });
+    }
 
     private void googleBtnClick() {
         btnGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                this.startSignInGoogle();
-            }
-
-            private void startSignInGoogle() {
-                startActivityForResult(
-                        AuthUI.getInstance()
-                                .createSignInIntentBuilder()
-                                .setAvailableProviders(Collections.singletonList(new
-                                        AuthUI.IdpConfig.GoogleBuilder().build()))
-                                .setIsSmartLockEnabled(false, true)
-                                .build(),
-                        RC_SIGN_IN);
-            }
+            public void onClick(View v) { startSignInGoogle(); }
         });
     }
 
     private void facebookBtnClick() {
         btnFacecebook.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                this.startSignInActivitywithFacebook();
-            }
-
-            /**
-             * Create user in Firebase for Facebook
-             */
-            private void startSignInActivitywithFacebook() {
-                startActivityForResult(
-                        AuthUI.getInstance()
-                                .createSignInIntentBuilder()
-                                .setAvailableProviders(Collections.singletonList(new
-                                        AuthUI.IdpConfig.FacebookBuilder().build()))
-                                .setIsSmartLockEnabled(false, true)
-                                .build(),
-                        RC_SIGN_IN);
-            }
+            public void onClick(View v) { startSignInActivitywithFacebook(); }
         });
     }
 
-    // 2 - Show Snack Bar with a message
+    private void startSignInEmail() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Collections.singletonList(new
+                                AuthUI.IdpConfig.EmailBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    private void startSignInGoogle() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Collections.singletonList(new
+                                AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    /**
+     * Create user in Firebase for Facebook
+     */
+    private void startSignInActivitywithFacebook() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Collections.singletonList(new
+                                AuthUI.IdpConfig.FacebookBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
     private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
         Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
     }
 
-    // 3 - Method that handles response after SignIn Activity close
     private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
 
         IdpResponse response = IdpResponse.fromResultIntent(data);
