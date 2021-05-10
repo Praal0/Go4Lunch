45e57b3ea2d8952a69bf3eab2b24c5faaa9d706e
Praal0 <guerrero.dorian1@gmail.com>
2021-05-10 09:38:19 +0200

    P7_Update_Login
    Update : Use FireBase Auth
    Remove : All old auth create
    Add : new theme and logo

--- app/src/main/java/com/example/go4lunch/LoginActivity.java
+++ app/src/main/java/com/example/go4lunch/LoginActivity.java
@@ -1,143 +1,59 @@
 package com.example.go4lunch;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ErrorCodes;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Collections;
+import java.util.Arrays;
 
-public class LoginActivity extends AppCompatActivity {
+import base.BaseActivity;
 
-    private static final int RC_SIGN_IN = 123;
-    private Button btnGoogle;
-    private Button btnFacecebook;
-    private Button btnEmail;
-    private CoordinatorLayout coordinatorLayout;
+public class LoginActivity extends BaseActivity {
+
+    // FOR DATA
+    private static final int RC_SIGN_IN = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-        innit();
-        googleBtnClick();
-        facebookBtnClick();
-        emailBtnClick();
+        if (!this.isCurrentUserLogged()){
+            this.startSignInActivity();
+        }else {
+            launchMainActivity();
+        }
     }
-
-    @Nullable
-    protected FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
-
-    protected Boolean isCurrentUserLogged(){ return (this.getCurrentUser() != null); }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+        // Handle SignIn Activity response on activity result
     }
 
-    private void innit() {
-        btnGoogle = findViewById(R.id.google_btn);
-        btnFacecebook = findViewById(R.id.facebook_btn);
-        coordinatorLayout = findViewById(R.id.login_activity_constraint_layout);
-        btnEmail = findViewById(R.id.email_btn);
-    }
-
-    private void emailBtnClick() {
-        btnEmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {startSignInEmail();}
-        });
-    }
-
-    private void googleBtnClick() {
-        btnGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { startSignInGoogle(); }
-        });
-    }
-
-    private void facebookBtnClick() {
-        btnFacecebook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { startSignInActivitywithFacebook(); }
-        });
-    }
-
-    private void startSignInEmail() {
+    @SuppressLint("ResourceType")
+    private void startSignInActivity(){
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
-                        .setAvailableProviders(Collections.singletonList(new
-                                AuthUI.IdpConfig.EmailBuilder().build()))
+                        .setTheme(R.style.LoginTheme)
+                        .setLogo(R.drawable.meal_logo)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(), // EMAIL
+                                        new AuthUI.IdpConfig.GoogleBuilder().build(),      // GOOGLE
+                                        new AuthUI.IdpConfig.FacebookBuilder().build()))         // FACEBOOK
                         .setIsSmartLockEnabled(false, true)
                         .build(),
                 RC_SIGN_IN);
     }
+    // --------------------
+    // ACTION
+    // --------------------
 
-    private void startSignInGoogle() {
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(Collections.singletonList(new
-                                AuthUI.IdpConfig.GoogleBuilder().build()))
-                        .setIsSmartLockEnabled(false, true)
-                        .build(),
-                RC_SIGN_IN);
+    private void launchMainActivity() {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
-
-    /**
-     * Create user in Firebase for Facebook
-     */
-    private void startSignInActivitywithFacebook() {
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(Collections.singletonList(new
-                                AuthUI.IdpConfig.FacebookBuilder().build()))
-                        .setIsSmartLockEnabled(false, true)
-                        .build(),
-                RC_SIGN_IN);
-    }
-
-    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
-        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
-    }
-
-    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
-
-        IdpResponse response = IdpResponse.fromResultIntent(data);
-
-        if (requestCode == RC_SIGN_IN) {
-            if (resultCode == RESULT_OK) { // SUCCESS
-                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
-            } else { // ERRORS
-                if (response == null) {
-                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
-                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
-                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
-                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
-                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
-                }
-            }
-        }
-    }
-
-
-
-
 }
